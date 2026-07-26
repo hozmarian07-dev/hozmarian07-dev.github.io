2cf0e3100b2319a745e4fa6adf3b62bc7e44a46d
maariaaneek <hozmarian07@gmail.com>
2026-07-26 22:22:22 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,1 +1,27 @@
-const webhook = "https://discord.com/api/webhooks/1531020612875849819/-oh4IV2xXSkfdl1g2uplK2HtJ9bDoS4bXesF4_iYdpKnR9DCpi0rY0Lv3ypCP1H_SIEq";
+export default {
+async fetch(request) {
+
+if(request.method === "POST") {
+
+const data = await request.text();
+
+
+await fetch("TVŮJ_WEBHOOK", {
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:data
+});
+
+
+return new Response("OK");
+
+}
+
+
+return new Response("Apex API");
+
+}
+
+}
