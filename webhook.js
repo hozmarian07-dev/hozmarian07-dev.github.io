0372dc1e4744c90163c6e1264100b41ab1076975
maariaaneek <hozmarian07@gmail.com>
2026-07-26 22:22:51 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -6,7 +6,7 @@ if(request.method === "POST") {
 const data = await request.text();
 
 
-await fetch("TVŮJ_WEBHOOK", {
+await fetch("https://discord.com/api/webhooks/1531020612875849819/-oh4IV2xXSkfdl1g2uplK2HtJ9bDoS4bXesF4_iYdpKnR9DCpi0rY0Lv3ypCP1H_SIEq", {
 method:"POST",
 headers:{
 "Content-Type":"application/json"
